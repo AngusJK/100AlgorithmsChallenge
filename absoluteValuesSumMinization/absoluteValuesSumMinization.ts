2a48dc03765d04d79e6f0f0e9a801a0cb4f1fff8
Angus Kellett <anguskellett@hotmail.com>
2021-04-09 18:32:59 +0000

    fills in body of function

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,12 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+    if(a.length % 2 === 0) {
+        return a[(a.length/2)-1]
+    } else {
+        return a[(a.length-1)/2]
+    }
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
